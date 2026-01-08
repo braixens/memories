2d50b3e75c306e4ac14256dedfc05d2f8b875b22
braixens <sylveons@mail.com>
2026-01-08 10:34:14 -0500

    added _blank/noref to links. Also replaced icons with svgs

--- src/components/Links.tsx
+++ src/components/Links.tsx
@@ -1,8 +1,8 @@
 import github from '../assets/Github.svg'
-import discord from '../assets/discord.png'
+// import discord from '../assets/discord.png'
 import back from '../assets/backicon.png'
-import osu from '../assets/osu.png'
-import tele from '../assets/tele.png'
+// import osu from '../assets/osu.png'
+// import tele from '../assets/tele.png'
 import cure1 from '../assets/pixel.gif'
 import cure2 from '../assets/cure2.gif'
 import cure3 from '../assets/cure3.webp'
@@ -31,17 +31,17 @@ const Links = ({ backClicked }) => {
                      backdropFilter: 'blur(4px)',
                      boxShadow: '0 8px 32px 0 rgba(31, 38, 135, 0.37)'
                  }}>
-                <a href="https://github.com/braixens">
+                <a href="https://github.com/braixens" rel="noreferrer" target="_blank">
                     <img src={github} alt="github" className="w-12 h-12"/>
                 </a>
-                <a href="https://discord.com/users/190235333768511489">
-                    <img src={discord} alt="discord" className="w-12 h-12"/>
+                <a href="https://discord.com/users/190235333768511489" rel="noreferrer" target="_blank">
+                    <img src='https://www.svgrepo.com/show/506463/discord.svg' alt="discord" className="w-12 h-12"/>
                 </a>
-                <a href="https://osu.ppy.sh/users/syI">
-                    <img src={osu} alt="osu" className="w-12 h-12"/>
+                <a href="https://osu.ppy.sh/users/syI" rel="noreferrer" target="_blank">
+                    <img src='https://upload.wikimedia.org/wikipedia/commons/1/1e/Osu%21_Logo_2016.svg' alt="osu" className="w-12 h-12"/>
                 </a>
-                <a href="https://t.me/sylveon">
-                    <img src={tele} alt="tele" className="w-12 h-12"/>
+                <a href="https://t.me/sylveon" rel="noreferrer" target="_blank">
+                    <img src='https://www.svgrepo.com/show/394493/telegram.svg' alt="tele" className="w-12 h-12"/>
                 </a>
             </div>
         </div>
